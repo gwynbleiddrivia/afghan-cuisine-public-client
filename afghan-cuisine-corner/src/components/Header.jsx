ef874cb1d86c787b62a86f908543dcd31bbc2db9
gwynbleiddrivia <yasinzme2@gmail.com>
2023-05-03 14:15:35 +0600

    firebase installed and config keys are now hidden by using the environment variable

--- afghan-cuisine-corner/src/components/Header.jsx
+++ afghan-cuisine-corner/src/components/Header.jsx
@@ -4,17 +4,17 @@ import {Link} from 'react-router-dom'
 const Header = () => {
 	return (
 		<div>
-			<div className="navbar bg-base-100">
+			<div className="navbar bg-base-100 flex justify-center">
 			  <div className="flex-1">
-			    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+			    <a className="btn btn-ghost normal-case text-xl">Afghan Cuisine Corner</a>
 			  </div>
 			  <div className="flex-none">
-			    <ul className="menu menu-horizontal px-1">
+			    <ul className="menu menu-horizontal px-1 flex justify-center gap-1">
 				    <Link className="btn btn-ghost" to="/">Home</Link>
 				    <Link className="btn btn-ghost"to="/blog">Blog</Link>
 				    <Link className="btn btn-ghost"to="/login">Login</Link>
 				    <Link className="btn btn-ghost"to="/register">Register</Link>
-			      <img className="w-1/4 h-1/4 rounded-xl" src="https://alanzo.ancorathemes.com/wp-content/uploads/2017/09/testi-2-150x150.jpg"/>
+			      <img className="w-10 h-10 rounded-xl" src="https://alanzo.ancorathemes.com/wp-content/uploads/2017/09/testi-2-150x150.jpg"/>
 			    </ul>
 			  </div>
 			</div>
